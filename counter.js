debcdab4ecd76a54edad127ee81a762c3264ed71
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2024-12-29 22:24:38 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,4 +1,4 @@
-function createCounter() {
+/* function createCounter() {
     let count = 0;
 
     return {
@@ -20,3 +20,4 @@ counter.increment();
 console.log(counter.getCount()); // Виведе: 2
 counter.reset();
 console.log(counter.getCount()); // Виведе: 0
+*/
