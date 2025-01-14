7658fdcb91eaba478e93c70a1a9110510815a5ff
Hard <125761273+HardProgrammerGod@users.noreply.github.com>
2025-01-14 21:40:54 +0200

    Create promise.js

---
+++ promise.js
@@ -0,0 +1,19 @@
+// day 543689348 of learning, i forgot promises
+
+/* console.log("Request data...")
+
+setTimeout(() => {
+    console.log("Preparing data...");
+
+    const backEndData = {
+        server:"goi",
+        port:2000,
+        statuss:"working"
+    }
+
+    setTimeout(() => {
+        backEndData.modif = true;
+
+        console.log("Data received", backEndData);
+    }, 2000);
+}, 2000); */
